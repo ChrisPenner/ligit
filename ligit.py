06fbb6b0f246741f5c9db25be14bec1dab12f0b0
Chris Penner <christopher.penner@gmail.com>
2015-06-06 23:04:21 -0600

    Clean up edge cases.

--- ligit.py
+++ ligit.py
@@ -5,28 +5,30 @@ import os
 from subprocess import call
 from tempfile import mkdtemp
 import shutil
+from shutil import ignore_patterns
 
 GIT_PREFIX = 'https://www.github.com/'
-BLACK   = '\033[30m'
-RED     = '\033[31m'
-GREEN   = '\033[32m'
-YELLOW  = '\033[33m'
-BLUE    = '\033[34m'
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
 MAGENTA = '\033[35m'
-CYAN    = '\033[36m'
-WHITE   = '\033[37m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
 DEFAULT = '\033[39m'
 
-RESET         = '\033[0m'
-BOLD          = '\033[1m'
-ITALICS       = '\033[2m'
-UNDERLINE     = '\033[4m'
-INVERSE       = '\033[7m'
+RESET = '\033[0m'
+BOLD = '\033[1m'
+ITALICS = '\033[2m'
+UNDERLINE = '\033[4m'
+INVERSE = '\033[7m'
 STRIKETHROUGH = '\033[9m'
 
 COMMENT = 'comment'
 REPO = 'repo'
 COMMAND = 'command'
+DEVNULL = open(os.devnull, 'w')
 
 
 def print_error(message):
@@ -40,16 +42,16 @@ def clone(user, project, tempdir, branch=None):
     url = GIT_PREFIX + user + '/' + project
     print 'url:', url
     project_dir = os.path.join(tempdir, project)
-    green('cloning %s into %s' % (url, project_dir))
-    status = call(['git', 'clone', url, project_dir])
+    green('cloning %s into %s' % (os.path.basename(url), project_dir))
+    status = call(['git', 'clone', url, project_dir],
+                  stdout=DEVNULL)
     if status != 0:
         print_error('Failed to clone %s, moving on.' % url)
-        return False, None;
+        return False, None
     if branch:
         prev_dir = os.getcwd()
         os.chdir(project_dir)
-        green('checking out branch: %s' % branch)
-        checkout_succeeded = call(['git', 'checkout', branch]) == 0
+        checkout_succeeded = call(['git', 'checkout', branch], stdout=DEVNULL) == 0
         os.chdir(prev_dir)
         if not checkout_succeeded:
             print_error("No branch/tag %s in project %s" % (branch, project))
@@ -57,14 +59,15 @@ def clone(user, project, tempdir, branch=None):
     return True, project_dir
 
 def move(src, dest):
-    green('moving %s to %s' % (src, dest))
+    green('moving %s to %s' % (os.path.basename(src), dest))
     if os.path.isfile(src):
         dest_dir = os.path.dirname(dest)
         if not os.path.isdir(dest_dir):
             os.makedirs(dest_dir)
         success = shutil.move(src, dest) == 0
     else:
-        success = shutil.copytree(src, dest) == 0
+        success = shutil.copytree(src, dest,
+                                  ignore=ignore_patterns('.git')) == 0
     return success
 
 def get_line_type(line):
@@ -94,11 +97,16 @@ with open(manifest_file) as f:
     failed = False
     project_dir = None
     clone_succeeded = False
+    commands_run = True
     for line in f.xreadlines():
         line_type = get_line_type(line)
         if line_type == COMMENT:
             continue
         if line_type == REPO:
+            if not commands_run:
+                src = project_dir
+                dest = os.path.join(manifest_dir, project)
+                move(src, dest)
             if '#' in line:
                 repo, branch = line.split('#')
                 repo, branch = repo.strip(), branch.strip()
@@ -109,8 +117,16 @@ with open(manifest_file) as f:
             user, project = user.strip(), project.strip()
             clone_succeeded, project_dir = clone(user=user, project=project,
                                               tempdir=tempdir, branch=branch)
+            if clone_succeeded:
+                commands_run = False
+                project_dest = os.path.join(manifest_dir, project)
+                if os.path.isdir(project_dest):
+                    print_error('Removing existing dir: %s' % project_dest)
+                    shutil.rmtree(project_dest)
+
 
         if line_type == COMMAND and clone_succeeded:
+            commands_run = True
             if '>' in line:
                 src, dest = line.split('>')
                 src, dest = src.strip(), dest.strip()
@@ -119,59 +135,10 @@ with open(manifest_file) as f:
             src = os.path.join(project_dir, src)
             dest = os.path.join(manifest_dir, project, dest)
             move(src, dest)
+    if not commands_run:
+        src = project_dir
+        dest = os.path.join(manifest_dir, project)
+        move(src, dest)
 
 print tempdir
 call(['rm', '-rf', tempdir])
-
-# move(){
-#     src=`cut -f1 -d\> <<< "$1" | tr -d ' '`
-#     dest=`cut -f2 -d\> <<< "$1" | tr -d ' '`
-#     if [ -d $dest ]; then
-#         echo "Folder at $dest already exists"
-#         # if [ $response = 'y' -o $response = 'Y' ]; then
-#         # echo rm -rf $dest
-#         # else
-#             # echo "Skipping over $dest"
-#         # fi
-#     fi
-#     mkdir -p "$dest"
-#     echo "Moving `basename $src` > $dest"
-#     if mv "$tempdir/$src" $dest; then
-#         return 0
-#     else
-#         echo "Failed moving '$src' to '$dest'"
-#         return 1
-#     fi
-# }
-
-
-# while IFS='' read -r line; do
-#     if [ "`tr -d ' ' <<< \"$line\" | cut -c1 `" = '#' ]; then
-#         continue
-#     fi
-#     if [ "`cut -c1 <<< \"$line\"`" = ' ' ]; then
-#         iscommand=0
-#     else
-#         iscommand=1
-#         if [ $tempdir ]; then
-#             rm -rf $tempdir
-#         fi
-#     fi
-
-#     if [ $iscommand = 0 -a "$error" = 1 ]; then
-#         continue
-#     fi
-
-#     if [ $iscommand = 1 ]; then
-#         if clone $line; then
-#             error=0
-#             echo "Successfully cloned $line"
-#         else
-#             error=1
-#             rm -rf $tempdir
-#         fi
-#     else
-#         move "$line"
-#     fi
-# done < $file
-# rm -rf $tempdir
